b64639f9b781a2dbd92b8a20dfe9b29d6bda7872
Tara Walenczyk <t.walenczy@gmail.com>
2024-07-05 22:17:34 -0400

    new

--- src/app/Home/HomeComponent.js
+++ src/app/Home/HomeComponent.js
@@ -16,10 +16,9 @@ export default function HomeComponent() {
 
   return (
     <div className="min-h-screen bg-white flex flex-col items-center justify-center">
-
       <div className="grid grid-cols-2 gap-4 w-full py-10 mb-6">
         <div className="flex items-center justify-center">
-          <p className='text-center text-4xl text-brown font-bold fadeIn'>
+          <p className="text-center text-4xl text-brown font-bold fadeIn">
             <span className="block" style={{ textAlign: 'left' }}>Tutoring</span>
             <span className="block" style={{ textAlign: 'left' }}>that will boost</span>
             <span className="block" style={{ textAlign: 'left' }}>confidence &amp; grades</span>
@@ -39,7 +38,7 @@ export default function HomeComponent() {
       </div>
 
       <div className="top-10 left-0 right-0 text-center z-30 mt-10">
-        <p className="text-4xl font-bold text-darkbrown" style={{ fontFamily: "Roboto, sans-serif" }}>
+        <p className="text-4xl font-bold text-darkbrown" style={{ fontFamily: 'Roboto, sans-serif' }}>
           Get Started Today
         </p>
       </div>
@@ -69,7 +68,7 @@ export default function HomeComponent() {
       <div className="flex flex-wrap items-center justify-center mt-10 mb-10">
         <div className="w-full lg:w-3/4 px-6 lg:px-0">
           <div className="bg-white px-6 py-4">
-            <p className="text-center text-4xl font-bold text-darkbrown mb-4" style={{ fontFamily: "Roboto, sans-serif", fontWeight: 'bold' }}>
+            <p className="text-center text-4xl font-bold text-darkbrown mb-4" style={{ fontFamily: 'Roboto, sans-serif', fontWeight: 'bold' }}>
               Subjects
             </p>
             <div className="flex flex-wrap text-gray-700 text-3xl">
@@ -87,13 +86,14 @@ export default function HomeComponent() {
       </div>
 
       <div className="text-center mt-10 p-6 border border-black rounded-md mb-10">
-        <p className="text-4xl font-bold text-darkbrown" style={{ fontFamily: "Roboto, sans-serif", color: '#FFD700' }}>
+        <p className="text-4xl font-bold text-darkbrown" style={{ fontFamily: 'Roboto, sans-serif', color: '#FFD700' }}>
           ★★★★★
         </p>
-        <p className="text-3xl text-gray-700" style={{ fontFamily: "Roboto, sans-serif" }}>5-star rating on <a href="https://www.wyzant.com/tutor/profile"><u>Wyzant</u></a></p>
-        <p className="text-lg" style={{ fontFamily: "Roboto, sans-serif" }}>(40 ratings)</p>
+        <p className="text-3xl text-gray-700" style={{ fontFamily: 'Roboto, sans-serif' }}>
+          5-star rating on <a href="https://www.wyzant.com/tutor/profile"><u>Wyzant</u></a>
+        </p>
+        <p className="text-lg" style={{ fontFamily: 'Roboto, sans-serif' }}>(40 ratings)</p>
       </div>
-
     </div>
   );
 }
